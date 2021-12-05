067bbfb52057878caacf91272a94ab2f035a8d70
Patrik Svensson <patrik.svensson@manyone.com>
2021-12-05 11:36:39 +0100

    fix: Cors

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const io = socketIO(server);
+const io = socketIO(server, {
+    cors: {
+        origin: "https://metervara-socket-io-client.netlify.app"
+    }
+});
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
 /* Broadcast server time to clients */
